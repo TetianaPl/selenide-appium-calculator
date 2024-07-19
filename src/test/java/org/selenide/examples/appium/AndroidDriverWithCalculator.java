0f07e30a45497d695842c827edc9a1424a416e4e
Tetiana_Plaksina <tetiana.plaksina@gmail.com>
2024-07-19 20:55:32 +0200

    Added 2 tests % and sqrt. Added device rotating.

--- src/test/java/org/selenide/examples/appium/AndroidDriverWithCalculator.java
+++ src/test/java/org/selenide/examples/appium/AndroidDriverWithCalculator.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.WebDriverProvider;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebDriver;
 
 import javax.annotation.CheckReturnValue;
@@ -26,12 +27,14 @@ public class AndroidDriverWithCalculator implements WebDriverProvider {
         options.setAppActivity("com.android.calculator2.Calculator");
         options.setNewCommandTimeout(Duration.ofSeconds(11));
         options.setFullReset(false);
+//        options.setOrientation(ScreenOrientation.LANDSCAPE);
 
         //Create AndroidDriver instance and connect to the Appium server.
         //It will launch the Calculator App in Android Device using the configurations specified in Desired Capabilities
 
         try {
-            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+//            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+            return new AndroidDriver(new URL("http://192.168.1.34:4723"), options);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/org/selenide/examples/appium/CalculatorTest.java
+++ src/test/java/org/selenide/examples/appium/CalculatorTest.java
@@ -2,16 +2,18 @@ package org.selenide.examples.appium;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.appium.AppiumDriverRunner;
+import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.appium.AppiumSelectors.byAttribute;
 
 /**
  * Thanks to <a href="https://github.com/mkpythonanywhereblog">Maryna Kolesnik</a> for this example!
@@ -41,5 +43,35 @@ public class CalculatorTest {
     $(By.id("digit_4")).click();
     $(By.id("eq")).click();
     $(By.id("result")).shouldHave(text("6"));
+  }
+  @Test
+  void calculatorPercents() {
+    AndroidDriver androidDriver = AppiumDriverRunner.getAndroidDriver();
+    androidDriver.rotate(ScreenOrientation.LANDSCAPE);
+    $(By.id("digit_5")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("op_mul")).click();
+    $(By.id("digit_1")).click();
+    $(By.id("digit_0")).click();
+    $(byAttribute("content-desc", "percent")).click();
+    $(By.id("eq")).click();
+    $(By.id("result")).shouldHave(text("50"));
+    androidDriver.rotate(ScreenOrientation.PORTRAIT);
+  }
+  @Test
+  void calculatorSquareRoot() {
+    $(byAttribute("content-desc", "square root")).click();
+    $(By.id("lparen")).click();
+    $(By.id("digit_1")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("op_add")).click();
+    $(By.id("digit_3")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("digit_0")).click();
+    $(By.id("rparen")).click();
+    $(By.id("eq")).click();
+    $(By.id("result")).shouldHave(text("20"));
   }
 }
